6078aa8392ffbf230654a4c7b5023f986b18913c
eniamrej7 <73618074+eniamrej7@users.noreply.github.com>
2025-12-10 01:22:16 -0500

    Add comments to Harvard CS50 hello.c
    Added comments saying this was my first lab assignment in the Harvard CS50 course. It is a simple Hello World program that a variable as a name to say hello to.

--- Harvard CS50 hello.c
+++ Harvard CS50 hello.c
@@ -1,3 +1,7 @@
+// This was my first lab assignment from the Harvard CS50 course and helped me to also  understand using Git Hub
+// This program outputs the phrase hello and the entered name variable
+// This program is written in c
+
 #include <cs50.h>
 #include <stdio.h>
 
